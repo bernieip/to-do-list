46c4efee6ceb187f8d252b89d5d28685a2bc6260
bernieip <bernie.ip414@gmail.com>
2020-02-16 14:06:51 -0500

    add git ignore, procfile, update ports

--- app.js
+++ app.js
@@ -144,9 +144,13 @@ app.post("/delete", function(req, res) {
     }
 });
 
-// create server on port 3000
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+// create server on port heroku port or port 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port, function() {
+    console.log("Server has started sucessfully.");
 });
 
 
